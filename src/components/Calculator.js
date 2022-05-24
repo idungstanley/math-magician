085634ad8f945b84fcae553f546817e1aecb931a
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-24 14:35:57 +0100

    Add style and jsx

---
+++ src/components/Calculator.js
@@ -0,0 +1,26 @@
+const Calculator = () => (
+  <section className="main">
+    <div className="display">0</div>
+    <div className="box">AC</div>
+    <div className="box">+/-</div>
+    <div className="box">%</div>
+    <div className="orange">÷</div>
+    <div className="box">7</div>
+    <div className="box">8</div>
+    <div className="box">9</div>
+    <div className="orange">x</div>
+    <div className="box">4</div>
+    <div className="box">5</div>
+    <div className="box">6</div>
+    <div className="orange">-</div>
+    <div className="box">1</div>
+    <div className="box">2</div>
+    <div className="box">3</div>
+    <div className="orange">+</div>
+    <div className="box zero">0</div>
+    <div className="box">.</div>
+    <div className="orange">=</div>
+  </section>
+);
+
+export default Calculator;
