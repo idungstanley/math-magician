5d75af0a75c75213f9dc5554f64732f582d93348
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-24 22:51:42 +0100

    Add class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Calculator from './Calculator';
-
+import './App.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return (
-      <Calculator />
-    );
+    return <Calculator />;
   }
 }
 
