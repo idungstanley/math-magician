55e3d7029d822e6cfe7407072be1558ec5e90ca4
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-27 11:11:15 +0100

    Add Hooks to my project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,105 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculator';
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleEvent = this.handleEvent.bind(this);
-  }
+const Calculator = () => {
+  const startState = {
+    total: 0,
+    next: null,
+    operation: null,
+  };
 
-  handleEvent = (event) => {
+  const [state, setState] = useState(startState);
+  const { total, next, operation } = state;
+
+  const handleEvent = (event) => {
     const name = event.target.textContent;
-    const updateState = calculate(this.state, name);
-    this.setState(updateState);
-  }
+    const updateState = calculate(state, name);
+    setState(updateState);
+  };
+  return (
+    <section className="main">
+      <div className="display">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <button className="box" type="button" id="AC" onClick={handleEvent}>
+        AC
+      </button>
+      <button type="button" className="box" id="+/-" onClick={handleEvent}>
+        +/-
+      </button>
+      <button type="button" className="box" id="%" onClick={handleEvent}>
+        %
+      </button>
+      <button type="button" className="orange" id="÷" onClick={handleEvent}>
+        ÷
+      </button>
+      <button type="button" className="box" id="7" onClick={handleEvent}>
+        7
+      </button>
+      <button type="button" className="box" id="8" onClick={handleEvent}>
+        8
+      </button>
+      <button type="button" className="box" id="9" onClick={handleEvent}>
+        9
+      </button>
+      <button type="button" className="orange" id="x" onClick={handleEvent}>
+        x
+      </button>
+      <button type="button" className="box" id="4" onClick={handleEvent}>
+        4
+      </button>
+      <button type="button" className="box" id="5" onClick={handleEvent}>
+        5
+      </button>
+      <button type="button" className="box" id="6" onClick={handleEvent}>
+        6
+      </button>
+      <button type="button" className="orange" id="-" onClick={handleEvent}>
+        -
+      </button>
+      <button type="button" className="box" id="1" onClick={handleEvent}>
+        1
+      </button>
+      <button type="button" className="box" id="2" onClick={handleEvent}>
+        2
+      </button>
+      <button type="button" className="box" id="3" onClick={handleEvent}>
+        3
+      </button>
+      <button type="button" className="orange" id="+" onClick={handleEvent}>
+        +
+      </button>
+      <button type="button" className="box zero" id="0" onClick={handleEvent}>
+        0
+      </button>
+      <button type="button" className="box" id="." onClick={handleEvent}>
+        .
+      </button>
+      <button type="button" className="orange" id="=" onClick={handleEvent}>
+        =
+      </button>
+    </section>
+  );
+};
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <section className="main">
-        <div className="display">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <button
-          className="box"
-          type="button"
-          id="AC"
-          onClick={this.handleEvent}
-        >
-          AC
-        </button>
-        <button
-          type="button"
-          className="box"
-          id="+/-"
-          onClick={this.handleEvent}
-        >
-          +/-
-        </button>
-        <button type="button" className="box" id="%" onClick={this.handleEvent}>
-          %
-        </button>
-        <button
-          type="button"
-          className="orange"
-          id="÷"
-          onClick={this.handleEvent}
-        >
-          ÷
-        </button>
-        <button type="button" className="box" id="7" onClick={this.handleEvent}>
-          7
-        </button>
-        <button type="button" className="box" id="8" onClick={this.handleEvent}>
-          8
-        </button>
-        <button type="button" className="box" id="9" onClick={this.handleEvent}>
-          9
-        </button>
-        <button type="button" className="orange" id="x" onClick={this.handleEvent}>
-          x
-        </button>
-        <button type="button" className="box" id="4" onClick={this.handleEvent}>
-          4
-        </button>
-        <button type="button" className="box" id="5" onClick={this.handleEvent}>
-          5
-        </button>
-        <button type="button" className="box" id="6" onClick={this.handleEvent}>
-          6
-        </button>
-        <button type="button" className="orange" id="-" onClick={this.handleEvent}>
-          -
-        </button>
-        <button type="button" className="box" id="1" onClick={this.handleEvent}>
-          1
-        </button>
-        <button type="button" className="box" id="2" onClick={this.handleEvent}>
-          2
-        </button>
-        <button type="button" className="box" id="3" onClick={this.handleEvent}>
-          3
-        </button>
-        <button type="button" className="orange" id="+" onClick={this.handleEvent}>
-          +
-        </button>
-        <button type="button" className="box zero" id="0" onClick={this.handleEvent}>
-          0
-        </button>
-        <button type="button" className="box" id="." onClick={this.handleEvent}>
-          .
-        </button>
-        <button type="button" className="orange" id="=" onClick={this.handleEvent}>
-          =
-        </button>
-      </section>
-    );
-  }
-}
+export default Calculator;
