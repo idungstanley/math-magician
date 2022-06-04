d29bc63bce20f7f124ddc7d240cbadf205ad4a43
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-04 10:17:39 +0100

    Add routing

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculator';
+import './calculator.css';
 // eslint-disable-next-line react/prefer-stateless-function
 const Calculator = () => {
   const baseState = {
@@ -17,70 +18,73 @@ const Calculator = () => {
     setState(updateState);
   };
   return (
-    <section className="main">
-      <div className="display">
-        {total}
-        {operation}
-        {next}
-      </div>
-      <button className="box" type="button" id="AC" onClick={handleEvent}>
-        AC
-      </button>
-      <button type="button" className="box" id="+/-" onClick={handleEvent}>
-        +/-
-      </button>
-      <button type="button" className="box" id="%" onClick={handleEvent}>
-        %
-      </button>
-      <button type="button" className="orange" id="÷" onClick={handleEvent}>
-        ÷
-      </button>
-      <button type="button" className="box" id="7" onClick={handleEvent}>
-        7
-      </button>
-      <button type="button" className="box" id="8" onClick={handleEvent}>
-        8
-      </button>
-      <button type="button" className="box" id="9" onClick={handleEvent}>
-        9
-      </button>
-      <button type="button" className="orange" id="x" onClick={handleEvent}>
-        x
-      </button>
-      <button type="button" className="box" id="4" onClick={handleEvent}>
-        4
-      </button>
-      <button type="button" className="box" id="5" onClick={handleEvent}>
-        5
-      </button>
-      <button type="button" className="box" id="6" onClick={handleEvent}>
-        6
-      </button>
-      <button type="button" className="orange" id="-" onClick={handleEvent}>
-        -
-      </button>
-      <button type="button" className="box" id="1" onClick={handleEvent}>
-        1
-      </button>
-      <button type="button" className="box" id="2" onClick={handleEvent}>
-        2
-      </button>
-      <button type="button" className="box" id="3" onClick={handleEvent}>
-        3
-      </button>
-      <button type="button" className="orange" id="+" onClick={handleEvent}>
-        +
-      </button>
-      <button type="button" className="box zero" id="0" onClick={handleEvent}>
-        0
-      </button>
-      <button type="button" className="box" id="." onClick={handleEvent}>
-        .
-      </button>
-      <button type="button" className="orange" id="=" onClick={handleEvent}>
-        =
-      </button>
-    </section>
+    <div className="flex">
+      <h2>Lets do some math</h2>
+      <section className="main">
+        <div className="display">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <button className="box" type="button" id="AC" onClick={handleEvent}>
+          AC
+        </button>
+        <button type="button" className="box" id="+/-" onClick={handleEvent}>
+          +/-
+        </button>
+        <button type="button" className="box" id="%" onClick={handleEvent}>
+          %
+        </button>
+        <button type="button" className="orange" id="÷" onClick={handleEvent}>
+          ÷
+        </button>
+        <button type="button" className="box" id="7" onClick={handleEvent}>
+          7
+        </button>
+        <button type="button" className="box" id="8" onClick={handleEvent}>
+          8
+        </button>
+        <button type="button" className="box" id="9" onClick={handleEvent}>
+          9
+        </button>
+        <button type="button" className="orange" id="x" onClick={handleEvent}>
+          x
+        </button>
+        <button type="button" className="box" id="4" onClick={handleEvent}>
+          4
+        </button>
+        <button type="button" className="box" id="5" onClick={handleEvent}>
+          5
+        </button>
+        <button type="button" className="box" id="6" onClick={handleEvent}>
+          6
+        </button>
+        <button type="button" className="orange" id="-" onClick={handleEvent}>
+          -
+        </button>
+        <button type="button" className="box" id="1" onClick={handleEvent}>
+          1
+        </button>
+        <button type="button" className="box" id="2" onClick={handleEvent}>
+          2
+        </button>
+        <button type="button" className="box" id="3" onClick={handleEvent}>
+          3
+        </button>
+        <button type="button" className="orange" id="+" onClick={handleEvent}>
+          +
+        </button>
+        <button type="button" className="box zero" id="0" onClick={handleEvent}>
+          0
+        </button>
+        <button type="button" className="box" id="." onClick={handleEvent}>
+          .
+        </button>
+        <button type="button" className="orange" id="=" onClick={handleEvent}>
+          =
+        </button>
+      </section>
+    </div>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <section className="head">
+      <h2>Welcome to our page</h2>
+      <p className="quote">
+        Welcome every morning with a smile. Look on the new day as another
+        special gift from your Creator, another golden opportunity to complete
+        what you were unable to finish yesterday. Be a self-starter. Let your
+        first hour set the theme of success and positive action that is certain
+        to echo through your entire day. Today will never happen again. Dont
+        waste it with a false start or no start at all. You were not born to
+        fail.
+      </p>
+    </section>
+  );
+}
+
+export default Home;
